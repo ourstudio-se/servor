9f826b14c48632e5546fde16358317824f9292fe
Peter <peter@ourstudio.se>
2020-05-29 14:31:29 +0200

    update

---
+++ cli.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const ourHttpServer = required('./index').default;
+
+(async () => {
+  const args = process.argv.slice(2);
+
+  const { root, port, ips, url } = await ourHttpServer({
+    root: args[0],
+    port: args[1] ? parseInt(args[1]) : undefined,
+  });
+
+  // Output server details to the console
+  console.log(`
+  🗂  Serving:\t${root}\n
+  🏡 Local:\t${url}
+  ${ips.map((ip) => `📡 Network:\thttp://${ip}:${port}`).join("\n  ")}
+  `);
+})();
